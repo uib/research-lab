c526a5bc7bfbdb099506e8cf291a666a67e51545
Jarle Bjørgeengen <jarle.bjorgeengen@usit.uio.no>
2017-05-31 08:42:41 +0200

    Add necessary modules for uh-iaas and TF kode provision on uh-iaas

---
+++ terraform/deployments/uh-iaas/variables.tf
@@ -0,0 +1,17 @@
+# Provider config
+variable "auth_url" {}
+variable "domain_name" {}
+variable "tenant_name" {}
+variable "user_name" {}
+variable "password" {}
+variable "region" {}
+variable "worker_node_flavor" {}
+variable "node_flavor" {}
+variable "coreos_image" {}
+variable "public_v4_network" {}
+variable "availability_zone" {}
+#variable "worker_count" {}
+# Cluster metadata
+variable "cluster_name" {}
+variable "cluster_dns_domain" {}
+
